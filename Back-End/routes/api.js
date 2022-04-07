f72d3e60db39c0b206ace845dd0bacaf9a3c71df
Mstars_lab1_05 <mstars_lab1_05@Mstars-lab1-05s-iMac.local>
2022-04-07 11:45:27 +0800

    add

--- Back-End/routes/api.js
+++ Back-End/routes/api.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const bookController = require("../controller/bookController")
-
+const bookController = require("../controller/bookController");
 
 //Endpoints
 
-router.get("/books",bookController.get_books);
-router.post("/createBooks",bookController.create);
-router.put("/updateBooks/id/:id",bookController.update);
-router.delete("/deleteBooks/id/:id",bookController.delete_book);
-
+router.get("/books", bookController.get_books);
+router.post("/createBooks", bookController.create);
+router.put("/updateBooks/id/:id", bookController.update);
+router.delete("/deleteBooks/id/:id", bookController.delete_book);
 
 module.exports = router;
