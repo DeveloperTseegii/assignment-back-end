2551a4f5d4cccce4aa6234905e7fa9aba7863706
Mstars_lab1_05 <mstars_lab1_05@Mstars-lab1-05s-iMac.local>
2022-04-18 13:00:07 +0800

    add

--- Back-End/index.js
+++ Back-End/index.js
@@ -1,17 +1,20 @@
-require("dotenv").config()
+require("dotenv").config();
 const dotenv = require("dotenv").config();
 const express = require("express");
-const cors = require("cors")
+const cors = require("cors");
 const apiRoutes = require("./routes/api");
 const connection = require("./database");
 const app = express();
-const PORT = 3000;
+const PORT = 3002;
 app.use(cors());
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
